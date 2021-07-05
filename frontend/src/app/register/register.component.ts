2426833fc40a2e0e1b65a46d2686ca10488a980a
Divesh <technodivesh@gmail.com>
2021-07-05 18:37:28 +0530

    adding frontend, Angular APP

---
+++ frontend/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+     this.form = this.formBuilder.group({
+      username: '',
+      email: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    console.log(this.form.getRawValue())
+    this.http.post('http://localhost:8000/api/signup', this.form.getRawValue())
+      .subscribe(() => this.router.navigate(['/login']));
+  }
+
+}
